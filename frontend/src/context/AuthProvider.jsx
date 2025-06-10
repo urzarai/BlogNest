54aa7f8eb1b1d9a58182dde3a8fdd2fe9d8906af
Urza Rai <raiurza@gmail.com>
2025-06-10 14:04:09 +0530

    Context

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
-export const AuthContext = React.createContext();
-
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
+export const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
+  const [blogs, setBlogs] = useState([]);
 
-    const [blogs, setBlogs] = useState();
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const {data} = await axios.get(
+          "http://localhost:4001/api/blogs/all-blogs"
+        );
+        console.log(data);
+        setBlogs(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBlogs();
+  }, []);
+  return (
+    <AuthContext.Provider value={{ blogs }}>{children}</AuthContext.Provider>
+  );
+};
 
-    return (
-        <AuthContext.Provider value={{}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+export const useAuth = () => useContext(AuthContext);
